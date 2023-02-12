c4424427a717b48aae06705c2c10bb22f1566391
daichi.sugiyama <caad12.di2@gmail.com>
2023-02-12 16:05:49 +0900

    #1 サイドバーMocを実装

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,22 @@
 <template>
+	<sidebar class="sidebar"></sidebar>
 	<v-container class="counter">
 		<router-view></router-view>
 	</v-container>
 </template>
+
+<script>
+import Sidebar from './components/layouts/sidebar.vue'
+
+export default {
+	components: {
+		Sidebar,
+	},
+}
+</script>
+
+<style scoped>
+	.sidebar {
+		float: left;
+	}
+</style>
