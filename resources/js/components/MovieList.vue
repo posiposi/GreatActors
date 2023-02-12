1e56f27a921ab2b48052b4cd554ef4e19f4884a8
daichi.sugiyama <caad12.di2@gmail.com>
2023-02-12 23:02:06 +0900

    #5 映画一覧テーブルの初期Mocを実装

--- resources/js/components/MovieList.vue
+++ resources/js/components/MovieList.vue
@@ -1,7 +1,31 @@
 <template>
-	<p>Top Page</p>
-	<h1>{{ count }}</h1>
-	<v-btn color="blue" @click="incrementCount()">Increment</v-btn>
+	<h1>映画一覧</h1>
+
+	<v-table>
+		<thead>
+			<tr>
+				<th class="text-left">
+					映画名
+				</th>
+				<th class="text-left">
+					公開年
+				</th>
+				<th class="text-left">
+					上映時間
+				</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr v-for="movie in movies" :key="movie.name">
+				<td>{{ movie.name }}</td>
+				<td>{{ movie.releaseYear }}</td>
+				<td>{{ movie.airTime }}</td>
+			</tr>
+		</tbody>
+	</v-table>
+
+	<!-- <h1>{{ count }}</h1>
+	<v-btn color="blue" @click="incrementCount()">Increment</v-btn> -->
 </template>
 
 <script setup>
@@ -10,4 +34,22 @@ const count = ref(0);
 const incrementCount = () => {
 	count.value++
 };
+
+const movies = [
+	{
+		name: 'アポロ13',
+		releaseYear: 1995,
+		airTime: 120
+	},
+	{
+		name: '孤高のメス',
+		releaseYear: 2010,
+		airTime: 110
+	},
+	{
+		name: 'スラムダンク',
+		releaseYear: 2022,
+		airTime: 130
+	},
+]
 </script>
