5e91668704d9e5587497a5249e7a0b5badf1c97f
daichi.sugiyama <caad12.di2@gmail.com>
2023-02-15 00:18:18 +0900

    #9 映画テーブルレコードの全件取得、疎通確認用テストコードを作成

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/movies', [MovieController::class, 'getAllMovieList']);
+Route::get('/movies', [MovieController::class, 'getAllMovieList'])->name('movies.index');
