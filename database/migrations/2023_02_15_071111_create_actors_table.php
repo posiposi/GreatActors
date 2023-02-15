ad2574fbfcf6b2e16112012df9ae999d1f1e0e1e
daichi.sugiyama <caad12.di2@gmail.com>
2023-02-15 22:22:44 +0900

    #23 idカラム作成コードを変更

--- database/migrations/2023_02_15_071111_create_actors_table.php
+++ database/migrations/2023_02_15_071111_create_actors_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('actors', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('actor_name');
             $table->timestamps();
         });
