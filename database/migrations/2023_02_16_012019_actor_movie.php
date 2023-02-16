13c152c4a9286bb5a87ee82a82f4461b2cdeb783
daichi.sugiyama <caad12.di2@gmail.com>
2023-02-16 14:33:47 +0900

    #24 映画-俳優中間テーブルを作成

---
+++ database/migrations/2023_02_16_012019_actor_movie.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * 映画-俳優中間テーブル作成マイグレーションを実行する
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actor_movie', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('actor_id');
+            $table->unsignedBigInteger('movie_id');
+            $table->boolean('role_id')->default(false);
+            $table->foreign('actor_id')->references('id')->on('actores')->onDelete('cascade');
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('actor_movie');
+    }
+};
