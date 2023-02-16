1f635c4d2888209189590abc910dd543ef808b87
daichi.sugiyama <caad12.di2@gmail.com>
2023-02-16 15:11:07 +0900

    #27 クラス説明のコメントを追加

--- app/Models/Actor.php
+++ app/Models/Actor.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 俳優モデル
+ */
 class Actor extends Model
 {
     use HasFactory;
