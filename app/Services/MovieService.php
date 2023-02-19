12576f91c26f7e8dc5b57080bf8341287e1e7986
daichi.sugiyama <caad12.di2@gmail.com>
2023-02-19 17:46:23 +0900

    #38 該当映画レコードの削除メソッドを実装

--- app/Services/MovieService.php
+++ app/Services/MovieService.php
@@ -17,4 +17,16 @@ class MovieService
 
         return response()->json(['movies' => $movies]);
     }
+
+    /**
+     * 該当の映画レコードを削除する
+     *
+     * @param integer $movie_id 削除対象の映画id
+     * @return void
+     */
+    public function deleteMovieRecord(int $movie_id): void
+    {
+        $movie = Movie::findOrFail($movie_id);
+        $movie->delete();
+    }
 }
