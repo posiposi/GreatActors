bc81d88c06c073774defbd93db1769180a115200
daichi.sugiyama <caad12.di2@gmail.com>
2023-02-20 14:21:31 +0900

    #38 該当映画レコード削除のAPIルーティングを設定

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/movies', [MovieController::class, 'getAllMovieList'])->name('movies.index');
+Route::get('/movies', [MovieController::class, 'getAllMovieList']);
+
+Route::delete('/{movieId}/delete', [MovieController::class, 'destroy']);
