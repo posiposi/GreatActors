95fed89e6350e2bbb16dce2fcff37c100071d9c5
daichi.sugiyama <caad12.di2@gmail.com>
2023-02-22 13:14:51 +0900

    #43 登録フォームページへのルーティングを設定

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,11 +1,19 @@
 import MovieList from './components/MovieList.vue';
+import RegistrationForm from './components/RegistrationForm.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
+	// 映画一覧ページ
 	{
 		path: '/',
 		name: 'MovieList',
 		component: MovieList,
+	},
+	// 登録フォームページ
+	{
+		path: '/registration',
+		name: 'RegistrationForm',
+		component: RegistrationForm,
 	},
 ];
 const router = createRouter({
