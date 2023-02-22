3643b3728a2faaee08dd89e3932c632be09b875f
daichi.sugiyama <caad12.di2@gmail.com>
2023-02-22 14:40:10 +0900

    #43 サイドバーのリンクを設定

--- resources/js/components/layouts/sidebar.vue
+++ resources/js/components/layouts/sidebar.vue
@@ -3,13 +3,24 @@
 		<v-layout>
 			<v-navigation-drawer image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg" permanent theme="dark">
 				<v-list nav>
-					<v-list-item prepend-icon="mdi-filmstrip" title="映画一覧" value="inbox" class="text-h4"></v-list-item>
-					<v-list-item prepend-icon="mdi-account-details" title="俳優一覧" value="supervisors"
-						class="text-h4"></v-list-item>
-					<v-list-item prepend-icon="mdi-plus-box" title="登録" value="clockin" class="text-h4"></v-list-item>
+					<v-list-item prepend-icon="mdi-filmstrip" class="text-h5">
+						<router-link class="router_link text-black" to="/">映画一覧</router-link>
+					</v-list-item>
+					<v-list-item prepend-icon="mdi-account-details" class="text-h5">
+						<router-link class="router_link text-black" to="#">俳優一覧</router-link>
+					</v-list-item>
+					<v-list-item prepend-icon="mdi-plus-box" class="text-h5">
+						<router-link class="router_link text-black" to="/registration">登録</router-link>
+					</v-list-item>
 				</v-list>
 			</v-navigation-drawer>
 			<v-main style="height: 250px"></v-main>
 		</v-layout>
 	</v-card>
 </template>
+
+<style>
+.router_link {
+	text-decoration: none;
+}
+</style>
