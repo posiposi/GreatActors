f46eb38330b5a62331945363a9dd6f50354301c8
daichi.sugiyama <caad12.di2@gmail.com>
2023-02-23 16:07:15 +0900

    #51 映画ModelにSoftDeletesトレイトを定義

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * 映画クラス
@@ -11,6 +12,7 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'movie_name',
