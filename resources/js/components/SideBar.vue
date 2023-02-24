a71cbdb518168c89140cd6203bf2d372b69cdc35
daichi.sugiyama <caad12.di2@gmail.com>
2023-02-24 10:29:37 +0900

    #54 ディレクトリ構成変更

--- resources/js/components/SideBar.vue
+++ resources/js/components/SideBar.vue
@@ -1,16 +1,16 @@
 <template>
 	<v-card>
 		<v-layout>
-			<v-navigation-drawer image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg" permanent theme="dark">
+			<v-navigation-drawer color="red-darken-4" permanent>
 				<v-list nav>
 					<v-list-item prepend-icon="mdi-filmstrip" class="text-h5">
-						<router-link class="router_link text-black" to="/">映画一覧</router-link>
+						<router-link class="router_link text-white" to="/">映画一覧</router-link>
 					</v-list-item>
 					<v-list-item prepend-icon="mdi-account-details" class="text-h5">
-						<router-link class="router_link text-black" to="#">俳優一覧</router-link>
+						<router-link class="router_link text-white" to="#">俳優一覧</router-link>
 					</v-list-item>
 					<v-list-item prepend-icon="mdi-plus-box" class="text-h5">
-						<router-link class="router_link text-black" to="/registration">登録</router-link>
+						<router-link class="router_link text-white" to="/registration">登録</router-link>
 					</v-list-item>
 				</v-list>
 			</v-navigation-drawer>
