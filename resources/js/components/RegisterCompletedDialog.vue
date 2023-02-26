529b52b3b17af258c3775f9bc6ca15379cc814f8
daichi.sugiyama <caad12.di2@gmail.com>
2023-02-26 17:01:21 +0900

    #47 映画登録完了ダイアログのコンポーネントを追加

---
+++ resources/js/components/RegisterCompletedDialog.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="completedDialog" width="auto">
+      <template>
+        <v-btn color="primary">
+          Open Dialog
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua.
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" block @click="completedDialog = false">Close Dialog</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const completedDialog = ref();
+</script>
