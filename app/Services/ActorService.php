4645514655f305cc09348f201481a72f1bdf189d
daichi.sugiyama <caad12.di2@gmail.com>
2023-02-28 18:19:47 +0900

    #70 俳優削除メソッドをサービスクラスに実装

--- app/Services/ActorService.php
+++ app/Services/ActorService.php
@@ -17,4 +17,16 @@ class ActorService
 
         return response()->json(['actors' => $actors]);
     }
+
+    /**
+     * 該当の俳優レコードを削除する
+     *
+     * @param integer $actor_id 削除対象の俳優id
+     * @return void
+     */
+    public function deleteActorRecord(int $actor_id): void
+    {
+        $actor = Actor::findOrFail($actor_id);
+        $actor->delete();
+    }
 }
